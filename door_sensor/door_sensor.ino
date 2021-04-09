cf1a1bee7159b2fed009003c25cc246d34d5e851
Daniel Rodriguez <danielfrancrodriguez@gmail.com>
2021-04-09 12:12:32 -0400

    added 10 pins, need 8 pin version

--- door_sensor/door_sensor.ino
+++ door_sensor/door_sensor.ino
@@ -15,14 +15,38 @@
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+const int DOOR_SENSOR_PIN_2 = 2;
+const int DOOR_SENSOR_PIN_3 = 3;
+const int DOOR_SENSOR_PIN_4 = 4;
+const int DOOR_SENSOR_PIN_5 = 5;
+const int DOOR_SENSOR_PIN_6 = 6;
 const int DOOR_SENSOR_PIN_7 = 7;
 const int DOOR_SENSOR_PIN_8 = 8;
+const int DOOR_SENSOR_PIN_9 = 9;
+const int DOOR_SENSOR_PIN_10 = 10;
+const int DOOR_SENSOR_PIN_11 = 11;
 
 int counter = 0;
+int doorState2 = LOW;
+int doorState3 = LOW;
+int doorState4 = LOW;
+int doorState5 = LOW;
+int doorState6 = LOW;
 int doorState7 = LOW;
 int doorState8 = LOW;
+int doorState9 = LOW;
+int doorState10 = LOW;
+int doorState11 = LOW;
+int prevDoorState2 = LOW;
+int prevDoorState3 = LOW;
+int prevDoorState4 = LOW;
+int prevDoorState5 = LOW;
+int prevDoorState6 = LOW;
 int prevDoorState7 = LOW;
 int prevDoorState8 = LOW;
+int prevDoorState9 = LOW;
+int prevDoorState10 = LOW;
+int prevDoorState11 = LOW;
 
 
 void setup() {
@@ -35,18 +59,41 @@ void setup() {
   delay(2000);
 
   Serial.begin(9600);
+  pinMode(DOOR_SENSOR_PIN_2, INPUT_PULLUP);
+  pinMode(DOOR_SENSOR_PIN_3, INPUT_PULLUP);
+  pinMode(DOOR_SENSOR_PIN_4, INPUT_PULLUP);
+  pinMode(DOOR_SENSOR_PIN_5, INPUT_PULLUP);
+  pinMode(DOOR_SENSOR_PIN_6, INPUT_PULLUP);
   pinMode(DOOR_SENSOR_PIN_7, INPUT_PULLUP);
   pinMode(DOOR_SENSOR_PIN_8, INPUT_PULLUP);
+  pinMode(DOOR_SENSOR_PIN_9, INPUT_PULLUP);
+  pinMode(DOOR_SENSOR_PIN_10, INPUT_PULLUP);
+  pinMode(DOOR_SENSOR_PIN_11, INPUT_PULLUP);
+  doorState2 = digitalRead(DOOR_SENSOR_PIN_2);
+  doorState3 = digitalRead(DOOR_SENSOR_PIN_3);
+  doorState4 = digitalRead(DOOR_SENSOR_PIN_4);
+  doorState5 = digitalRead(DOOR_SENSOR_PIN_5);
+  doorState6 = digitalRead(DOOR_SENSOR_PIN_6);
   doorState7 = digitalRead(DOOR_SENSOR_PIN_7);
   doorState8 = digitalRead(DOOR_SENSOR_PIN_8);
-  
+  doorState9 = digitalRead(DOOR_SENSOR_PIN_9);
+  doorState10 = digitalRead(DOOR_SENSOR_PIN_10);
+  doorState11 = digitalRead(DOOR_SENSOR_PIN_11);
 }
 
 void loop() {
 //  doorState7 = digitalRead(DOOR_SENSOR_PIN_7);
 //  doorState8 = digitalRead(DOOR_SENSOR_PIN_8);
+  doorOpened(prevDoorState2, doorState2, DOOR_SENSOR_PIN_2, 2);
+  doorOpened(prevDoorState3, doorState3, DOOR_SENSOR_PIN_3, 3);
+  doorOpened(prevDoorState4, doorState4, DOOR_SENSOR_PIN_4, 4);
+  doorOpened(prevDoorState5, doorState5, DOOR_SENSOR_PIN_5, 5);
+  doorOpened(prevDoorState6, doorState6, DOOR_SENSOR_PIN_6, 6);
   doorOpened(prevDoorState7, doorState7, DOOR_SENSOR_PIN_7, 7);
   doorOpened(prevDoorState8, doorState8, DOOR_SENSOR_PIN_8, 8);
+  doorOpened(prevDoorState9, doorState9, DOOR_SENSOR_PIN_9, 9);
+  doorOpened(prevDoorState10, doorState10, DOOR_SENSOR_PIN_10, 10);
+  doorOpened(prevDoorState11, doorState11, DOOR_SENSOR_PIN_11, 11);
 
   display.clearDisplay();               // Clear buffer
 //  delay(2000);                          // Delay by ms where 1000 ms = 1 second
