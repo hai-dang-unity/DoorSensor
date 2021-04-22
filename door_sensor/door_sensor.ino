2b376daab1ef4976d9bb6fca61b4fd91f5c5b67b
Daniel Rodriguez <danielfrancrodriguez@gmail.com>
2021-04-22 12:59:02 -0400

    save state branch

--- door_sensor/door_sensor.ino
+++ door_sensor/door_sensor.ino
@@ -1,3 +1,4 @@
+#include <EEPROM.h> // bringing in eeprom library
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -6,7 +7,7 @@
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-// The pins for I2C are defined by the Wire-library. 
+// The pins for I2C are defined by the Wire-library.
 // On an arduino UNO:       A4(SDA), A5(SCL)
 // On an arduino MEGA 2560: 20(SDA), 21(SCL)
 // On an arduino LEONARDO:   2(SDA),  3(SCL), ...
@@ -15,6 +16,10 @@
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+//const unsigned long day = 10000; // 10 second test, 86400000 when ready
+//unsigned long mytime = millis();// getting time for day condition below
+const int ADDRESS_EEPROM = 1;
+int value;
 const int DOOR_SENSOR_PIN_2 = 2;
 const int DOOR_SENSOR_PIN_3 = 3;
 const int DOOR_SENSOR_PIN_4 = 4;
@@ -51,7 +56,7 @@ int prevDoorState11 = LOW;
 
 void setup() {
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)){
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
     exit(0);
   }
@@ -79,11 +84,13 @@ void setup() {
   doorState9 = digitalRead(DOOR_SENSOR_PIN_9);
   doorState10 = digitalRead(DOOR_SENSOR_PIN_10);
   doorState11 = digitalRead(DOOR_SENSOR_PIN_11);
+
+  counter = EEPROM.read(ADDRESS_EEPROM);  
 }
 
 void loop() {
-//  doorState7 = digitalRead(DOOR_SENSOR_PIN_7);
-//  doorState8 = digitalRead(DOOR_SENSOR_PIN_8);
+  //  doorState7 = digitalRead(DOOR_SENSOR_PIN_7);
+  //  doorState8 = digitalRead(DOOR_SENSOR_PIN_8);
   doorOpened(prevDoorState2, doorState2, DOOR_SENSOR_PIN_2, 2);
   doorOpened(prevDoorState3, doorState3, DOOR_SENSOR_PIN_3, 3);
   doorOpened(prevDoorState4, doorState4, DOOR_SENSOR_PIN_4, 4);
@@ -96,27 +103,37 @@ void loop() {
   doorOpened(prevDoorState11, doorState11, DOOR_SENSOR_PIN_11, 11);
 
   display.clearDisplay();               // Clear buffer
-//  delay(2000);                          // Delay by ms where 1000 ms = 1 second
+  //  delay(2000);                          // Delay by ms where 1000 ms = 1 second
   delay(1);
   display.setTextSize(1);               // Normal 1:1 pixel scale
   display.setTextColor(SSD1306_WHITE);  // Draw white text
-  display.setCursor(0,0);               // Start at top-left corner
+  display.setCursor(0, 0);              // Start at top-left corner
   display.cp437(true);                  // Use full 256 char 'Code Page 437' font
   display.println(F("Door Opened Count"));    // Text to be shown and added to display buffer
-  
+
   display.println("Count: " + String(counter));
 
   display.display();
   delay(1);
-//  delay(1000);
-  
-}
+  //  delay(1000);
 
+  
+
+}
+// 86400000 is a day
 void doorOpened(int &prevDoorState, int &currentDoorState, int doorSensorPin, int pin) {
   prevDoorState = currentDoorState;
   currentDoorState = digitalRead(doorSensorPin);
   if (prevDoorState == LOW && currentDoorState == HIGH) {
     Serial.println("Door is opened: " + String(pin));
     counter += 1;
+    EEPROM.write(ADDRESS_EEPROM, counter);
+    value = EEPROM.read(ADDRESS_EEPROM);
+    Serial.print(ADDRESS_EEPROM);
+    Serial.print("\t");
+    Serial.print(value);
+    Serial.println();
+    //if (mytime % day == 0) { //testing with 10 seconds - ideally counter value gets recorded every 10 seconds
+      //for (int i = 0 ; i < 3; i++) { //i++ instead of i + 1, i < 90 for less than 90 days?
   }
 }
